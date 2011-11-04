1f217b59f1f4ed59e93a2a5ec914464885c23ee7
Milos Dinic <milos@mozilla.com>
2011-11-04 21:38:56 +0100

    Adding new branch; moving projects var to new file, moving css to new file

---
+++ projects-holder.js
@@ -0,0 +1,14 @@
+var projects = { // {{{
+	'webifyme': 'Webify Me (Desktop)',
+	'spark': 'Spark (Mobile)',
+	'dstudio': 'MDN Demo Studio',
+	'webowonder': 'Web O\'Wonder (Demo Microsite)',
+	'markup': 'Mark Up (Desktop)',
+	'tparty': 'Twitter Party (Desktop)',
+	'joinmozilla': 'New \'Join Mozilla\' page on mozilla.org',
+	'syncacc': 'New Services account page',
+	'fbbadges': 'Facebook Badges',
+	'glow': 'Global Map a.k.a. Glow - the new download stats page',
+	'fbwebhero': 'Facebook Web Hero',
+	'sparkeol': 'Spark EOL (End of Life)',
+} // }}}
