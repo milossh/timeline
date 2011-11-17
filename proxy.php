5f101cee38e477148b8c23e9d76222a307a05faa
Milos <milos@mozilla.com>
2011-11-17 15:59:34 +0100

    changing calendar source to stas' calendar again

--- proxy.php
+++ proxy.php
@@ -1,6 +1,6 @@
 <?php
 
 header('Content-type: text/plain');
-echo file_get_contents("https://mail.mozilla.com/home/mdinic@mozilla.com/l10n-releases");
+echo file_get_contents("https://mail.mozilla.com/home/smalolepszy@mozilla.com/timeline");
 
 ?>
